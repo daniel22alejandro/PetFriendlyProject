ab28f3af59446612e811d8b6052172642dc6b545
daniel22alejandro <danniel10yara@gmail.com>
2024-06-25 15:52:09 -0500

    trabajo finalizado

--- frontend_PetFriendly/src/components/ListaComponent.jsx
+++ frontend_PetFriendly/src/components/ListaComponent.jsx
@@ -54,48 +54,46 @@ const ListarComponent = () => {
         }
     };
 
-        // Función para contar mascotas por género
-        const contarMachosYHembras = (mascotas) => {
-            const totalMascotas = mascotas.length;
-            const machos = mascotas.filter(mascota => mascota.gender_id?.name_genders === 'Macho').length;
-            const hembras = mascotas.filter(mascota => mascota.gender_id?.name_genders === 'Hembra').length;
-            setConteoMascotas({ totalMascotas, machos, hembras });
-        };
+    // Función para contar mascotas por género
+    const contarMachosYHembras = (mascotas) => {
+        const totalMascotas = mascotas.length;
+        const machos = mascotas.filter(mascota => mascota.gender_id?.name_genders === 'Macho').length;
+        const hembras = mascotas.filter(mascota => mascota.gender_id?.name_genders === 'Hembra').length;
+        setConteoMascotas({ totalMascotas, machos, hembras });
+    };
     
-        const handleConsultar = () => {
-            return `Total Mascotas: ${conteoMascotas.totalMascotas}, Machos: ${conteoMascotas.machos}, Hembras: ${conteoMascotas.hembras}`;
-        };
+    const handleConsultar = () => {
+        return `Total Mascotas: ${conteoMascotas.totalMascotas}, Machos: ${conteoMascotas.machos}, Hembras: ${conteoMascotas.hembras}`;
+    };
 
     return (
         <div className="flex relative bg-[url('../imgs/bg.svg')] w-[400px] h-[785px] justify-center items-center flex-col">
-            
             <div className="flex absolute flex-row text-white top-10 gap-10">
                 <p>Administrar Mascotas</p>
                 <Link to='/' className="bg-[url('../imgs/btn-close.svg')] w-[34px] h-[34px] flex absolute left-56 cursor-pointer"></Link>
             </div>
             <Link to='/añadir' className="bg-[url('../imgs/btn-add.svg')] w-[360px] h-[50px] flex absolute cursor-pointer top-32"></Link>
             {error && <p>{error}</p>}
+            
+            <div className="top-40 gap-10 flex bg-gray-400 w-[360px] h-24 rounded-2xl items-center">
+                <p className="text-sky-700">{handleConsultar()}</p>
+            </div>
+            
             {mascotas && mascotas.map((mascota, index) => (
-                <div key={mascota._id} className={`flex absolute bg-gray-400 w-11/12 h-24 rounded-2xl ${index === 0 ? 'top-52' : index === 1 ? 'top-80' : index === 2 ? 'bottom-64' : index === 3 ? 'bottom-36' : 'bottom-8'} items-center hover:bg-gray-500`}>
+                <div key={mascota._id} className={`flex bg-gray-400 w-11/12 h-24 rounded-2xl ${index === 0 ? 'top-80' : index === 1 ? 'top-110' : index === 2 ? 'top-140' : index === 3 ? 'top-170' : `top-${170 + (index - 3) * 30}`} items-center hover:bg-gray-500`}>
                     <img src={`http://localhost:3000/img/${mascota.photo}`} alt={mascota.name} className="w-20 h-20 object-cover" />
                     <div className="flex absolute flex-col left-24">
                         <p className="text-sky-700 font-semibold">{mascota.name || 'Nombre no disponible'}</p>
                         <p className="text-sky-700">{mascota.race_id?.name_races || 'Raza no disponible'}</p>
                         <p className="text-sky-700">{mascota.category_id?.name_categories || 'Categoría no disponible'}</p>
-                        {/* Considera cómo manejar género, según tus datos */}
                     </div>
                     <Link to={`/consulta/${mascota._id}`} className="flex absolute bg-[url('../imgs/btn-show.svg')] w-[34px] h-[34px] right-28 cursor-pointer"></Link>
                     <Link to={`/editar/${mascota._id}`} className="flex absolute bg-[url('../imgs/btn-edit.svg')] w-[34px] h-[34px] right-16 cursor-pointer"></Link>
                     <div className="flex absolute bg-[url('../imgs/btn-delete.svg')] w-[34px] h-[34px] right-4 cursor-pointer" onClick={() => handleDelete(mascota._id)}></div>
                 </div>
             ))}
-    <div className="bg-gray-400 w-[360px] h-24 rounded-2xl items-center">
-        <p className="text-sky-700">{handleConsultar()}</p>
-    </div>
         </div>
     );
-}
-
-
+};
 
 export default ListarComponent;
